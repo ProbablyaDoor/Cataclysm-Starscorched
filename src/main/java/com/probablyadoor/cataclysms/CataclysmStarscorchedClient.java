be56cf3bdbc70de1e38b59d381d5ce9b99d9645b
BackDoor1F6AA <dom.689.095@gmail.com>
2026-07-14 12:33:52 -0700

    Added Fabric Mod Template and Client File

---
+++ src/main/java/com/probablyadoor/cataclysms/CataclysmStarscorchedClient.java
@@ -0,0 +1,10 @@
+package com.probablyadoor.cataclysms;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CataclysmStarscorchedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
